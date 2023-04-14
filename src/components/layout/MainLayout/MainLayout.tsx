25775d2771a54bbe30c4c39e8b7f1d2dc5c4b374
Ruslan Kozhushko <rkozhushko@griddynamics.com>
2023-04-14 13:08:16 -0400

    feat: add condition of check is nav link active;

--- src/components/layout/MainLayout/MainLayout.tsx
+++ src/components/layout/MainLayout/MainLayout.tsx
@@ -2,6 +2,7 @@ import { FC, PropsWithChildren } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { Roboto } from 'next/font/google';
+import { useRouter } from 'next/router';
 import classNames from 'classnames';
 
 import classes from './MainLayout.module.scss';
@@ -16,6 +17,8 @@ interface Props {
 }
 
 export const MainLayout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
+  const { pathname } = useRouter();
+
   return (
     <>
       <Head>
@@ -23,15 +26,30 @@ export const MainLayout: FC<PropsWithChildren<Props>> = ({ children, title }) =>
       </Head>
 
       <nav className={classNames(roboto.className, classes.nav)}>
-        <Link href="/" className={classes.link}>
+        <Link
+          href="/"
+          className={classNames(classes.link, {
+            [classes.linkActive]: pathname === '/',
+          })}
+        >
           Home
         </Link>
 
-        <Link href="/categories" className={classes.link}>
+        <Link
+          href="/categories"
+          className={classNames(classes.link, {
+            [classes.linkActive]: pathname === '/categories',
+          })}
+        >
           Categories
         </Link>
 
-        <Link href="/about-us" className={classes.link}>
+        <Link
+          href="/about-us"
+          className={classNames(classes.link, {
+            [classes.linkActive]: pathname === '/about-us',
+          })}
+        >
           About Us
         </Link>
       </nav>
