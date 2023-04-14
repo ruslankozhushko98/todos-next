4c8555b42e623ed57a02f3fb7a3751b616bda5d9
Ruslan Kozhushko <rkozhushko@griddynamics.com>
2023-04-14 06:00:41 -0400

    feat: add progress bar;

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress
+        height={3}
+        startPosition={0.3}
+        stopDelayMs={200}
+        color="#29D"
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
