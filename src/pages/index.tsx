f7859d1dab53cccd1542e9e9f0e354d4cf4e86ac
Ruslan Kozhushko <rkozhushko@griddynamics.com>
2023-04-14 05:37:09 -0400

    fix: fix;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
-import Head from 'next/head';
-
 import { MainLayout } from '@/components/layout/MainLayout/MainLayout';
 
 export default function Home() {
   return (
-    <MainLayout>
-      <Head>
-        <title>Home</title>
-      </Head>
-
+    <MainLayout title="Home">
       <h1>Home</h1>
     </MainLayout>
   );
