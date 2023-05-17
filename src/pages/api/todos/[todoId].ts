a8d77fe2ead9fe12642b45c3b2fdc460d6964920
Ruslan Kozhushko <ruslankozhushko98@gmail.com>
2023-05-17 18:02:42 -0400

    feat: add updating the progress after modifying todos' isDone;

--- src/pages/api/todos/[todoId].ts
+++ src/pages/api/todos/[todoId].ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { PostgrestError, PostgrestSingleResponse } from '@supabase/supabase-js';
 
 import { supabase } from '@/libs/config/supabase';
-import { Todo } from '@/models';
+import { Category, Todo } from '@/models';
 
 type Data = PostgrestSingleResponse<Todo | null>;
 type ErrorData = PostgrestError | null;
@@ -21,12 +21,47 @@ export default async function handler(
   }
 
   if (req.method === 'PUT') {
-    const response = await supabase.from('todos')
+    const todoUpdated = await supabase.from('todos')
       .update(req.body)
       .eq('id', req.query.todoId)
+      .select()
       .single<Todo>();
 
-    return res.status(200).json(response);
+    const categoryDetails = await supabase.from('categories')
+      .select('*, todos(*)')
+      .eq('id', todoUpdated.data?.category_id)
+      .single<Category>();
+
+    const progress = Number(
+      (
+        (Number(
+          categoryDetails.data?.todos?.reduce(
+            (accumulator, current) => accumulator + Number(current.isDone),
+            0,
+          ),
+        ) / Number(categoryDetails.data?.todos?.length)) * 100
+      ).toFixed(0),
+    );
+
+    const updatedCategory = await supabase.from('categories')
+      .update({
+        ...categoryDetails.data,
+        progress,
+        todos: undefined,
+      })
+      .eq('id', categoryDetails.data?.id)
+      .select()
+      .single<Category>();
+
+    const error = todoUpdated.error
+      || categoryDetails.error
+      || updatedCategory.error;
+
+    if (error) {
+      return res.status(500).json(error);
+    }
+
+    return res.status(200).json(todoUpdated);
   }
 
   if (req.method === 'DELETE') {
